5441a040b73fdb412033d0c6213f28bd181eadba
jvillegas1021 <jvillegas1021@yahoo.com>
2026-04-07 12:03:15 -0400

    Add files via upload

---
+++ pipelines/daily_pitcher_stats_update.py
@@ -0,0 +1,17 @@
+from data_extract_functions.extract_data_from_database import pitcher_seasonal_data_statcast, pitcher_seasonal_data_statsapi
+from data_transform_functions.process_functions import process_starting_pitcher_stats
+from data_load_functions.load_data_to_database import push_pitcher_data_to_sql_upsert_player_id
+
+
+def run_daily_pitcher_stats_update():
+    #extract
+    pitcher_statsapi = pitcher_seasonal_data_statsapi()
+    pitcher_statcast = pitcher_seasonal_data_statcast()
+    #transform
+    pitcher_current_year_stats_df = process_starting_pitcher_stats(pitcher_statsapi, pitcher_statcast)
+    #load
+    table_name = 'active_pitcher_stats'
+    push_pitcher_data_to_sql_upsert_player_id(table_name, pitcher_current_year_stats_df)
+
+if __name__ == "__main__":
+    run_daily_pitcher_stats_update()
