1a6495b22711f016a282b1c6b113807f8c8b881c
jvillegas1021 <jvillegas1021@yahoo.com>
2026-04-07 12:02:51 -0400

    Add files via upload

--- data_load_functions/load_data_to_database.py
+++ data_load_functions/load_data_to_database.py
@@ -86,6 +86,26 @@ def push_pitcher_data_to_sql_upsert(table_name: str, team_data_df: pd.DataFrame)
             )
             conn.execute(stmt)
 
+def push_pitcher_data_to_sql_upsert_player_id(table_name: str, team_data_df: pd.DataFrame):
+    engine = get_engine()
+
+    # 1. Create table if it doesn't exist
+    team_data_df.head(0).to_sql(table_name, con=engine, if_exists="append", index=False)
+
+    # 2. Reflect table
+    metadata = MetaData()
+    table = Table(table_name, metadata, autoload_with=engine)
+
+    # 3. UPSERT each row
+    with engine.begin() as conn:
+        for _, row in team_data_df.iterrows():
+            stmt = insert(table).values(row.to_dict())
+            stmt = stmt.on_conflict_do_update(
+                index_elements=['xMLBAMID'],   # your unique key
+                set_=row.to_dict()           # update all columns
+            )
+            conn.execute(stmt)
+
 def push_data_to_sql_replace(table_name: str, data_df: pd.DataFrame):
     engine = get_engine()
     
