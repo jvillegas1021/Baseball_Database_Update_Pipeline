0cf834deeb78e8bbe9139336f90d63dde6919912
jvillegas1021 <jvillegas1021@yahoo.com>
2026-04-17 09:06:00 -0400

    Update run_daily_morning_update.py

--- data_scheduler/run_daily_morning_update.py
+++ data_scheduler/run_daily_morning_update.py
@@ -4,6 +4,7 @@ from pipelines.daily_batter_statcast_update  import run_daily_batter_statcast_up
 from pipelines.daily_batter_statsapi_update  import run_daily_batter_statsapi_update
 from pipelines.daily_pitcher_stats_update  import run_daily_pitcher_stats_update
 from pipelines.daily_pitcher_current_season_stats_update import run_daily_pitcher_current_season_stats_update
+from pipelines.daily_pitcher_recent_form_update import run_starting_pitchers_recent_form_update
 
 def run_one_time_stats_update():
     
@@ -13,6 +14,7 @@ def run_one_time_stats_update():
     run_daily_batter_statsapi_update()
     run_daily_pitcher_stats_update()
     run_daily_pitcher_current_season_stats_update()
+    run_starting_pitchers_recent_form_update()
 
 if __name__ == "__main__":
     run_one_time_stats_update()
